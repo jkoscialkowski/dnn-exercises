d67ccb3bb16861619d3478e863cdef1e304ed53d
Koscial <jan.koscialkowski@gmail.com>
2019-06-17 23:28:16 +0200

    File structure for task 1 created

---
+++ task-1-convnet/network.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+
+class OwnBatchNorm(nn.Parameter):
+    def __init__(self):
+        pass
+
+
+class ConvDenseNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, *input):
+        pass
