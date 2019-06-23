29dc3479a02cb8138a085965fd7ad0bafdb7d9c3
Koscial <jan.koscialkowski@gmail.com>
2019-06-23 16:17:36 +0200

    Finished, network overfitting a bit, but fixable

--- task_1_convnet/network.py
+++ task_1_convnet/network.py
@@ -1,13 +1,15 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from torch.nn.parameter import Parameter
 
 
 class BatchNormDense(nn.Module):
-    def __init__(self, num_features, eps):
+    def __init__(self, num_features, eps=1e-8):
         super().__init__()
         self.num_features = num_features
+        self.eps = eps
         self.gamma = Parameter(torch.Tensor(num_features))
         self.beta = Parameter(torch.Tensor(num_features))
         self.reset_parameters()
@@ -24,17 +26,56 @@ class BatchNormDense(nn.Module):
 
 
 class BatchNormConv(nn.Module):
-    def __init__(self):
+    def __init__(self, num_channels, eps=1e-8):
         super().__init__()
-        pass
+        self.num_channels = num_channels
+        self.eps = eps
+        self.gamma = Parameter(torch.Tensor(num_channels))
+        self.beta = Parameter(torch.Tensor(num_channels))
+        self.reset_parameters()
 
-    def forward(self, *input):
-        pass
+    def reset_parameters(self):
+        nn.init.ones_(self.gamma)
+        nn.init.zeros_(self.beta)
+
+    def forward(self, x):
+        means = x.mean(dim=(0, 2, 3))
+        variances = x.var(dim=(0, 2, 3))
+        x = (x.permute(0, 2, 3, 1) - means) / torch.sqrt(variances + self.eps)
+        x = self.gamma * x + self.beta
+        return x.permute(0, 3, 1, 2)
 
 
 class ConvDenseNN(nn.Module):
-    def __init__(self):
+    def __init__(self, layer_list):
         super().__init__()
+        self.layer_names = []
+        for layer in layer_list:
+            self.layer_names.append(layer[0])
+            setattr(self, layer[0], layer[1])
 
-    def forward(self, *input):
+        self.layer_checker()
+
+    def layer_checker(self):
         pass
+
+    def forward(self, x):
+        # Boolean for determining whether linear layers were reached
+        seen_linear = False
+
+        for i, ln in enumerate(self.layer_names):
+            layer = getattr(self, ln)
+            if isinstance(layer, nn.Linear) and not seen_linear:
+                seen_linear = True
+                x = x.view(x.shape[0], -1)
+
+            x = layer(x)
+            # Apply ReLU after BatchNorm if not last layer
+            if isinstance(layer, (BatchNormDense, BatchNormConv)
+                          ) and not i == len(self.layer_names) - 1:
+                x = F.relu(x)
+
+        return x
+
+
+
--- task_1_convnet/train.py
+++ task_1_convnet/train.py
@@ -2,32 +2,90 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+from livelossplot import PlotLosses
 from torch.utils.data import DataLoader
+
+if torch.cuda.is_available():
+    DEVICE = 'cuda'
+else:
+    DEVICE = 'cpu'
 
 
 class Trainer:
-    def __init__(self, model, max_epochs, batch_size):
+    def __init__(self, model, max_epochs, batch_size, learning_rate=1e-3):
         self.model = model
+        self.model = self.model.to(DEVICE)
         self.max_epochs = max_epochs
         self.batch_size = batch_size
 
         # Fix optimizer to Adam and loss to CrossEntropy
-        self.optimizer = optim.Adam()
-        self.loss_fn = nn.CrossEntropyLoss()
+        self.optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+        self.loss_fn = nn.CrossEntropyLoss(reduction='sum')
+
+    @staticmethod
+    def count_goods(y_pred, y_true):
+        _, indices = y_pred.max(dim=1)
+        return torch.sum(indices == y_true).item()
 
     def train(self, train_ds, valid_ds, plot_loss=True):
+        # Initialize plotting
+        if plot_loss:
+            liveloss = PlotLosses()
+
         # Initialize DataLoaders
-        tdl = DataLoader(train_ds, batch_size=self.batch_size)
-        vdl = DataLoader(valid_ds, batch_size=len(valid_ds))
+        tdl = DataLoader(train_ds, batch_size=self.batch_size, pin_memory=True)
+        vdl = DataLoader(valid_ds, batch_size=self.batch_size, shuffle=False,
+                         pin_memory=True)
+
+        # Lists for losses
+        train_losses, valid_losses = [], []
+        # Lists for accuracies
+        train_accs, valid_accs = [], []
 
         # Iterate over epochs
         for epoch in range(self.max_epochs):
+            # Logs for livelossplot
+            logs = {}
 
+            batch_losses = []
+            batch_count_goods = []
             # Iterate over batches
             for idx_batch, batch in enumerate(tdl):
-                pred = self.model(batch['image'])
-                loss = self.loss_fn(pred, batch['y'])
+                x = batch[0].to(DEVICE)
+                y = batch[1].to(device=DEVICE, dtype=torch.long)
+                pred = self.model(x)
+                loss = self.loss_fn(pred, y)
+                batch_losses.append(loss.item())
+                # Accuracy
+                with torch.no_grad():
+                    batch_count_goods.append(
+                        self.count_goods(pred, y)
+                    )
                 loss.backward()
                 self.optimizer.step()
                 self.optimizer.zero_grad()
 
+            # Save train loss and accuracy for the epoch
+            train_losses.append(sum(batch_losses) / len(train_ds))
+            train_accs.append(sum(batch_count_goods) / len(train_ds))
+
+            # Compute and save validation loss and accuracy for the epoch
+            with torch.no_grad():
+                v_batch_losses, v_batch_count_goods = [], []
+                for idx_batch, batch in enumerate(vdl):
+                    x = batch[0].to(DEVICE)
+                    y = batch[1].to(device=DEVICE, dtype=torch.long)
+                    pred = self.model(x)
+                    loss = self.loss_fn(pred, y)
+                    v_batch_losses.append(loss.item())
+                    v_batch_count_goods.append(self.count_goods(pred, y))
+                valid_losses.append(sum(v_batch_losses) / len(valid_ds))
+                valid_accs.append(sum(v_batch_count_goods) / len(valid_ds))
+
+            if plot_loss:
+                logs['log loss'] = train_losses[epoch]
+                logs['val_log loss'] = valid_losses[epoch]
+                logs['accuracy'] = train_accs[epoch]
+                logs['val_accuracy'] = valid_accs[epoch]
+                liveloss.update(logs)
+                liveloss.draw()
